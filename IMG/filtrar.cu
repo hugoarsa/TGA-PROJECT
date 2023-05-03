2777671d9fa18d6f8a9f529e6e789ac2cf742696
hugoarsa <hugo.aransan.tech@gmail.com>
2023-05-03 11:35:48 +0200

    commit inicial con ejemplo del agustín

---
+++ IMG/filtrar.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+char *fileIN, *fileOUT;
+unsigned char *image;
+int width, height, pixelWidth; //meta info de la imagen
+
+
+
+int main(int argc, char** argv)
+{
+  // Ficheros de entrada y de salida 
+  if (argc == 3) { fileIN = argv[1]; fileOUT = argv[2]; }
+  else { printf("Usage: ./exe fileIN fileOUT\n"); exit(0); }
+
+
+  printf("Reading image...\n");
+  image = stbi_load(fileIN, &width, &height, &pixelWidth, 0);
+  if (!image) {
+    fprintf(stderr, "Couldn't load image.\n");
+     return (-1);
+  }
+  printf("Image Read. Width : %d, Height : %d, nComp: %d\n",width,height,pixelWidth);
+
+  printf("Filtrando\n");
+	//SECUENCIAL BLANCO Y NEGRO:
+	for(int i=0;i<width*height*3;i=i+3){
+		image[i]=image[i];
+		image[i+1]=image[i+1];
+		image[i+2]=0;
+	}
+  printf("Escribiendo\n");
+  //ESCRITURA DE LA IMAGEN EN SECUENCIAL
+  stbi_write_png(fileOUT,width,height,pixelWidth,image,0);
+
+}
+
